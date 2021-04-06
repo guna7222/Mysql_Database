4802ae63402ad265aa740141bfa91d3e66f35a96
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-04-06 16:54:18 +0530

    Update MySQL.sql

--- SQL/MySQL.sql
+++ SQL/MySQL.sql
@@ -1,1 +1,15 @@
-select * from employees;
+/*
+SQL stands for structured query language, and
+SQL is the standard language for dealing with relational database and,
+SQL is used to add, update, search, delete database records
+*/
+
+-- single line comments in MySQL
+/*
+Multiline comments in MySQL
+*/
+
+-- Organised collection of data stored in an electronic format. (Database)
+
+-- MySQL is a freely avaliable open source relational database management system that uses sql. (MySQL)
+
