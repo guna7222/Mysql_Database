b047f639ede62a9412eb0f2bd8ae2cc31b252c07
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-04-11 13:08:38 +0530

    Update create-delete-database.sql

--- create-alter-database/create-delete-database.sql
+++ create-alter-database/create-delete-database.sql
@@ -1,2 +1,5 @@
 CREATE DATABASE employees; -- create, database are keywords to create database
 -- employees is a database name
+
+DROP DATABASE employees; -- this statement deletes the DATABASE
+-- DROP is the keyword
