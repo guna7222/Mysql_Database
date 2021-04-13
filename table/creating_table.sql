eddd5f7930ab424664fa76436b5c1dce4122b661
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-04-13 10:19:46 +0530

    Update creating_table.sql

--- table/creating_table.sql
+++ table/creating_table.sql
@@ -1,3 +1,7 @@
+-- creating database 
+CREATE DATABASE user;
+USE user
+
 -- creating a table 
 CREATE TABLE users(
 u_id int,
@@ -5,4 +9,9 @@ u_name varchar(25),
 u_address varchar(50),
 u_mobile_number int,
 u_gender varchar(10),
+PRIMARY KEY(u_id)
 );
+
+-- inserting data into users table using insert into keyword
+
+INSERT INTO users(u_id, u_name, u_address, u_mobile_number, u_gender) VALUES(1, 'gunasekhar', 'bengaluru', '6498', 'male') 
