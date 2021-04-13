db035548102a9c480703fa97d367ca94acafd788
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-04-13 10:29:46 +0530

    Update select.sql

--- table/select/select.sql
+++ table/select/select.sql
@@ -1,1 +1,19 @@
+CREATE TABLE users(
+u_id int,
+u_name varchar(25),
+u_address varchar(50),
+u_mobile_number int,
+u_gender varchar(10),
+PRIMARY KEY(u_id)
+);
 
+-- inserting data into users table using insert into keyword
+
+INSERT INTO users(u_id, u_name, u_address, u_mobile_number, u_gender) VALUES(1, 'gunasekhar', 'bengaluru', '6498', 'male') 
+
+SELECT * FROM users; -- it will display all the rows in the given table.
+-- select command is used to extract data from a database.
+
+SELECT u_id, u_name from users; -- another way of selecting only specified columns.
+
+SELECT DISTINCT u_gender from users; -- SELECT DISTINCT statement prints only unique values in the columns. 
