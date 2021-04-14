42b2618c1f94d4998f55a5571cdf442654271b14
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-04-14 09:13:20 +0530

    Create alter_table_statement.sql

---
+++ alter_table/alter_table_statement.sql
@@ -0,0 +1,8 @@
+-- alter table statement is used to add, delete or modify columns in an existing table. 
+/*
+adding a new column to an existing table.
+syntax:- 
+ALTER TABLE table_name add column_name data_type
+*/
+
+ALTER TABLE users add age int 
